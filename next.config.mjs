5a4cbbc7812daee0c18e8b2ed523b3f388ff66a3
Jaspreet Singh <jaspreetsodhi02@gmail.com>
2025-12-26 11:14:01 +0530

    feat: enhanced ui

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,11 @@ const nextConfig = {
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: '*',
+            value: 'https://prosessed-b2b-ecommerce.vercel.app',
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
           },
           {
             key: 'Access-Control-Allow-Methods',
